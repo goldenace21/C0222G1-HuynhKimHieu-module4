b4756d088ff843fb18afa2403eede199a27457a6
HUYNH KIM HIEU <89566149+goldenace21@users.noreply.github.com>
2022-06-15 21:05:20 +0700

    up

--- _02_spring_controller/exercise/calculator/src/main/java/com/example/controller/CalculatorController.java
+++ _02_spring_controller/exercise/calculator/src/main/java/com/example/controller/CalculatorController.java
@@ -3,8 +3,8 @@ package com.example.controller;
 import com.example.service.ICalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CalculatorController {
@@ -18,12 +18,12 @@ public class CalculatorController {
     }
 
     @GetMapping("/calculator")
-    public String calculator(String number1, String number2, String operator, Model model) {
+    public String calculator(String number1, String number2, String operator, RedirectAttributes redirectAttributes) {
         String result = iCalculatorService.caculator(number1, number2, operator);
-        model.addAttribute("number1", number1);
-        model.addAttribute("number2", number2);
-        model.addAttribute("result", result);
-        model.addAttribute("operator", operator);
-        return "/home";
+        redirectAttributes.addFlashAttribute("number1", number1);
+        redirectAttributes.addFlashAttribute("number2", number2);
+        redirectAttributes.addFlashAttribute("result", result);
+        redirectAttributes.addFlashAttribute("operator", operator);
+        return "redirect:/";
     }
 }
